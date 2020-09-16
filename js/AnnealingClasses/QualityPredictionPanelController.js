ce7b248684a9e01cec7e3fbab3f0ff009b9733f2
Aditya Nittala <nittala@cs.uni-saarland.de>
2020-09-16 11:31:57 +0200

    New prediction quality panel on the main canvas and components of the canvas can be updated now.

--- js/AnnealingClasses/QualityPredictionPanelController.js
+++ js/AnnealingClasses/QualityPredictionPanelController.js
@@ -1,5 +1,5 @@
 var QualityPredictionPanelController = ( function(){
-    var emg_quality_bar, eda_quality_bar, ecg_quality_bar;
+    var emg_quality_bar, eda_quality_bar, ecg_quality_bar, overall_quality_bar;
     var brachioradialis_muscle, fcr_muscle, pq_muscle, pl_muscle, fcu_muscle;
     var br_muscle_selected = false, fcr_muscle_selected = false, pl_selected = false, pq_selected = false , fcu_selected = false;
     var g, quality_status;
@@ -8,7 +8,7 @@ var QualityPredictionPanelController = ( function(){
 
     function init(){
         g = main_canvas.group();
-        quality_status= Snap.load("./resources/status_svg_v2.svg", function (loadedFragment){
+        quality_status= Snap.load("./resources/status_svg_v6.svg", function (loadedFragment){
             g.append( loadedFragment );
             var myMatrix = new Snap.Matrix();
             myMatrix.translate(0,270);
@@ -16,14 +16,35 @@ var QualityPredictionPanelController = ( function(){
 
             g.attr({transform: myMatrix});
             //
-            brachioradialis_muscle = loadedFragment.select('#Brachiordialis_copy *');
-            fcr_muscle = loadedFragment.select('#fcr_muscle *');
-            pl_muscle = loadedFragment.select('#palmaris_longus *');
-            pq_muscle = loadedFragment.select('#pronator_quadratus');
-            fcu_muscle = loadedFragment.select('#fcu_muscle');
+            // brachioradialis_muscle = loadedFragment.select('#Brachiordialis_copy *');
+            // fcr_muscle = loadedFragment.select('#fcr_muscle *');
+            // pl_muscle = loadedFragment.select('#palmaris_longus *');
+            // pq_muscle = loadedFragment.select('#pronator_quadratus');
+            // fcu_muscle = loadedFragment.select('#fcu_muscle');
+            //
+            // ecg_status_heart = g.selectAll('#ecg_heart_status *').attr({ stroke: '#2b9e1f', fill: '#2b9e1f', opacity:0.75});
+            // g.selectAll('#fcr_muscle *').attr({ stroke: '#9e0e0e', opacity:0.75 });
+            // g.selectAll('#fcu_muscle *').attr({ stroke: '#9e0e0e', opacity:0.75 });
+            // g.selectAll('#Brachiordialis_copy *').attr({ stroke: '#9e0e0e', opacity:0.75 });
+            // g.selectAll('#pronator_quadratus *').attr({ stroke:'#9e0e0e', fill: '#9e0e0e', opacity:0.75 });
+            // g.selectAll('#palmaris_longus *').attr({ stroke: '#9e0e0e', opacity:0.75 });
+            // eda_status_skin = g.selectAll('#eda_status *').attr({ stroke: '#2b9e1f', fill: '#2b9e1f', opacity:0.75});
 
-            ecg_status_heart = loadedFragment.select('#ecg_heart_status');
-            eda_status_skin = loadedFragment.select('#eda_status');
+            // var emg_quality_percentage = g.selectAll('#emg_quality_percentage *');
+            // emg_quality_percentage.attr({text:50+"%"});
+            //
+            // emg_quality_bar = g.select('#emg_quality_bar_2_ *');
+            // var emg_quality_bar_width = emg_quality_bar.getBBox().width *  (1 - 0.5);
+            // emg_quality_bar.attr({width:emg_quality_bar_width});
+            //
+            // var ecg_quality_percentage = g.selectAll('#ecg_quality_percentage *');
+            // ecg_quality_percentage.attr({text:50+"%"});
+            //
+            // ecg_quality_bar = g.selectAll('#ecg_quality_bar *');
+            // var ecg_quality_bar_width = emg_quality_bar.getBBox().width *  (1 - 0.5);
+            // ecg_quality_bar.attr({width:ecg_quality_bar_width});
+
+
 
 
             main_canvas.append( g );
@@ -33,7 +54,7 @@ var QualityPredictionPanelController = ( function(){
     }
     
     function updateQualityStatus() {
-        quality_status= Snap.load("./resources/status_svg_v2.svg", function (loadedFragment){
+        quality_status= Snap.load("./resources/status_svg_v6.svg", function (loadedFragment){
 
             g.clear();
             g.append( loadedFragment );
@@ -44,15 +65,33 @@ var QualityPredictionPanelController = ( function(){
             myMatrix.scale(0.32);
 
             g.attr({transform: myMatrix});
-            loadedFragment.selectAll('#ecg_heart_status *').attr({ stroke: '#2b9e1f', fill: '#2b9e1f', opacity:0.75});
-            loadedFragment.selectAll('#fcr_muscle *').attr({ stroke: '#9e0e0e', opacity:0.75 });
+            g.selectAll('#ecg_heart_status *').attr({ stroke: '#2b9e1f', fill: '#2b9e1f', opacity:0.75});
+            g.selectAll('#eda_status *').attr({ stroke: '#2b9e1f', fill: '#2b9e1f', opacity:0.75});
+            g.selectAll('#fcr_muscle *').attr({ stroke: '#9e0e0e', opacity:0.75 });
 
-            var emg_quality_percentage = loadedFragment.select("#emg_quality_percentage");
+
+            var emg_quality_percentage = g.select('#emg_quality_percentage');
             emg_quality_percentage.attr({text:50+"%"});
 
-            emg_quality_bar = loadedFragment.select("#emg_quality_bar");
+            emg_quality_bar = g.select('#emg_quality_bar');
             var emg_quality_bar_width = emg_quality_bar.getBBox().width *  (1 - 0.5);
             emg_quality_bar.attr({width:emg_quality_bar_width});
+
+            var ecg_quality_percentage = g.select('#ecg_quality_percentage');
+            ecg_quality_percentage.attr({text:50+"%"});
+
+            ecg_quality_bar = g.select('#ecg_quality_bar');
+            var ecg_quality_bar_width = ecg_quality_bar.getBBox().width *  (1 - 0.5);
+            ecg_quality_bar.attr({width:ecg_quality_bar_width});
+
+            overall_quality_bar = g.select('#overall_quality_bar');
+            var overall_quality_bar_width = overall_quality_bar.getBBox().width *  (1 - 0.5);
+            overall_quality_bar.attr({width:overall_quality_bar_width});
+
+            var area_text_block = g.select('#area_text');
+            area_text_block.attr({text:123.45+""});
+
+
 
 
             main_canvas.append( g );
