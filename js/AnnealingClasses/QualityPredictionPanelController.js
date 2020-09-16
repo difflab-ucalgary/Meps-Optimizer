353557a1820c2cda9bf23127f084dc52340e20ea
Aditya Nittala <nittala@cs.uni-saarland.de>
2020-09-16 15:57:56 +0200

    Prediction quality updated.

--- js/AnnealingClasses/QualityPredictionPanelController.js
+++ js/AnnealingClasses/QualityPredictionPanelController.js
@@ -53,7 +53,7 @@ var QualityPredictionPanelController = ( function(){
 
     }
     
-    function updateQualityStatus() {
+    function updateQualityStatus(electrodeSet, forearmTrapezoidpoints, weights, main_canvas) {
         quality_status= Snap.load("./resources/status_svg_v6.svg", function (loadedFragment){
 
             g.clear();
@@ -65,31 +65,123 @@ var QualityPredictionPanelController = ( function(){
             myMatrix.scale(0.32);
 
             g.attr({transform: myMatrix});
-            g.selectAll('#ecg_heart_status *').attr({ stroke: '#2b9e1f', fill: '#2b9e1f', opacity:0.75});
-            g.selectAll('#eda_status *').attr({ stroke: '#2b9e1f', fill: '#2b9e1f', opacity:0.75});
-            g.selectAll('#fcr_muscle *').attr({ stroke: '#9e0e0e', opacity:0.75 });
+            // g.selectAll('#ecg_heart_status *').attr({ stroke: '#2b9e1f', fill: '#2b9e1f', opacity:0.75});
+            // g.selectAll('#eda_status *').attr({ stroke: '#2b9e1f', fill: '#2b9e1f', opacity:0.75});
+            // g.selectAll('#fcr_muscle *').attr({ stroke: '#9e0e0e', opacity:0.75 });
+
+
+
+
+            layoutEnergy = EnergyCalculator.GetSystemEnergyForArea(electrodeSet, forearmTrapezoidpoints, weights, main_canvas);
+
+            var emg_quality_fcr = 1 - EnergyCalculator.GetLayoutEnergy().FCRScore[2];
+            if(emg_quality_fcr > 0.6){
+                g.selectAll('#fcr_muscle *').attr({ stroke: '#2b9e1f', opacity:0.75 });
+            }else if(emg_quality_fcr < 0.3){
+                g.selectAll('#fcr_muscle *').attr({ stroke: '#9e0e0e', opacity:0.75 });
+            }else{
+                g.selectAll('#fcr_muscle *').attr({ stroke: '#FDD835', opacity:0.75 });
+            }
+
+            var emg_quality_br = 1 - EnergyCalculator.GetLayoutEnergy().BrScore[2];
+            if(emg_quality_br > 0.6){
+                g.selectAll('#Brachiordialis_copy *').attr({ stroke: '#2b9e1f', opacity:0.75 });
+            }else if(emg_quality_br < 0.3){
+                g.selectAll('#Brachiordialis_copy *').attr({ stroke: '#9e0e0e', opacity:0.75 });
+            }else{
+                g.selectAll('#Brachiordialis_copy *').attr({ stroke: '#FDD835', opacity:0.75 });
+            }
+
+            var emg_quality_pl = 1 - EnergyCalculator.GetLayoutEnergy().PLScore[2];
+            if(emg_quality_pl > 0.6){
+                g.selectAll('#palmaris_longus *').attr({ stroke: '#2b9e1f', opacity:0.75 });
+            }else if(emg_quality_pl < 0.3){
+                g.selectAll('#palmaris_longus *').attr({ stroke: '#9e0e0e', opacity:0.75 });
+            }else{
+                g.selectAll('#palmaris_longus *').attr({ stroke: '#FDD835', opacity:0.75 });
+            }
+
+            var emg_quality_pq = 1 - EnergyCalculator.GetLayoutEnergy().PQScore[2];
+            if(emg_quality_pq > 0.6){
+                g.selectAll('#pronator_quadratus *').attr({ fill:'#2b9e1f', stroke: '#2b9e1f', opacity:0.75 });
+            }else if(emg_quality_pq < 0.3){
+                g.selectAll('#pronator_quadratus *').attr({ fill:'#9e0e0e', stroke: '#9e0e0e', opacity:0.75 });
+            }else{
+                g.selectAll('#pronator_quadratus *').attr({ fill:'#FDD835', stroke: '#FDD835', opacity:0.75 });
+            }
+
+            var emg_quality_fcu = 1 - EnergyCalculator.GetLayoutEnergy().FCUScore[2];
+            if(emg_quality_fcu > 0.6){
+                g.selectAll('#fcu_muscle *').attr({ fill:'#2b9e1f', stroke: '#2b9e1f', opacity:0.75 });
+            }else if(emg_quality_fcu < 0.3){
+                g.selectAll('#fcu_muscle *').attr({ fill:'#9e0e0e', stroke: '#9e0e0e', opacity:0.75 });
+            }else{
+                g.selectAll('#fcu_muscle *').attr({ fill:'#FDD835', stroke: '#FDD835', opacity:0.75 });
+            }
+
+
+
+
+
+            if(EnergyCalculator.GetLayoutEnergy().EDAScore[1] > 0.66){
+                g.selectAll('#eda_status *').attr({ stroke: '#9e0e0e', fill: '#9e0e0e', opacity:0.75});
+            }else if(EnergyCalculator.GetLayoutEnergy().EDAScore[1] < 0.33){
+                g.selectAll('#eda_status *').attr({ stroke: '#2b9e1f', fill: '#2b9e1f', opacity:0.75  });
+            }else{
+                g.selectAll('#eda_status *').attr({ stroke: '#FDD835', fill: '#FDD835', opacity:0.75 });
+            }
+
+
+            if(1 - EnergyCalculator.GetLayoutEnergy().ECGScore > 0.65){
+                g.selectAll('#ecg_heart_status *').attr({ stroke: '#2b9e1f', fill: '#2b9e1f', opacity:0.75});
+
+            }else if(1 - EnergyCalculator.GetLayoutEnergy().ECGScore < 0.3){
+                g.selectAll('#ecg_heart_status *').attr({ stroke: '#9e0e0e', fill: '#9e0e0e', opacity:0.75  });
+            }else{
+                g.selectAll('#ecg_heart_status *').attr({ stroke: '#FDD835', fill: '#FDD835', opacity:0.75 });
+            }
 
 
             var emg_quality_percentage = g.select('#emg_quality_percentage');
-            emg_quality_percentage.attr({text:50+"%"});
+            emg_quality_percentage.attr({text:((1 - EnergyCalculator.GetLayoutEnergy().EMGScore)* 100).toFixed(1)+"%"});
 
             emg_quality_bar = g.select('#emg_quality_bar');
-            var emg_quality_bar_width = emg_quality_bar.getBBox().width *  (1 - 0.5);
+            var emg_quality_bar_width = emg_quality_bar.getBBox().width *  (1 - EnergyCalculator.GetLayoutEnergy().EMGScore);
             emg_quality_bar.attr({width:emg_quality_bar_width});
 
+
+            var eda_quality_percentage = g.select('#eda_quality_percentage');
+            eda_quality_percentage.attr({text:((1 - EnergyCalculator.GetLayoutEnergy().EDAScore[1])* 100).toFixed(1)+"%"});
+
+            eda_quality_bar = g.select('#eda_quality_bar');
+            var eda_quality_bar_width = eda_quality_bar.getBBox().width *  (1 - EnergyCalculator.GetLayoutEnergy().EDAScore[1]);
+            eda_quality_bar.attr({width:eda_quality_bar_width});
+
+
             var ecg_quality_percentage = g.select('#ecg_quality_percentage');
-            ecg_quality_percentage.attr({text:50+"%"});
+            ecg_quality_percentage.attr({text:((1 - EnergyCalculator.GetLayoutEnergy().ECGScore)* 100).toFixed(1)+"%"});
 
             ecg_quality_bar = g.select('#ecg_quality_bar');
-            var ecg_quality_bar_width = ecg_quality_bar.getBBox().width *  (1 - 0.5);
+            var ecg_quality_bar_width = ecg_quality_bar.getBBox().width *  (1 - EnergyCalculator.GetLayoutEnergy().ECGScore);
             ecg_quality_bar.attr({width:ecg_quality_bar_width});
 
+
+
+            var overall_quality_percentage = g.select('#overall_quality_percentage');
+            overall_quality_percentage.attr({text:((1 - layoutEnergy)* 100).toFixed(1)+"%"});
+
             overall_quality_bar = g.select('#overall_quality_bar');
-            var overall_quality_bar_width = overall_quality_bar.getBBox().width *  (1 - 0.5);
+            var overall_quality_bar_width = overall_quality_bar.getBBox().width *  (1 -layoutEnergy);
             overall_quality_bar.attr({width:overall_quality_bar_width});
 
             var area_text_block = g.select('#area_text');
-            area_text_block.attr({text:123.45+""});
+
+
+            var hull_points = ElectrodeLayout.GetConvexHull(electrodeSet);
+            //var input_shape_coords = InputShapeObject.GetShapeCoords();
+            hull_points = Utils.Convert2DArrayTo1D(hull_points);
+            var hull_area = Utils.GetPolygonArea(Utils.GetXCoords(hull_points), Utils.GetYCoords(hull_points), hull_points.length/2);
+            area_text_block.attr({text:(hull_area/100).toFixed(2)+""});
 
 
 
@@ -107,8 +199,8 @@ var QualityPredictionPanelController = ( function(){
             init();
             return;
         },
-        UpdateQualityStatus: function(){
-            updateQualityStatus();
+        UpdateQualityStatus: function(electrodeSet, forearmTrapezoidpoints, weights, main_canvas){
+            updateQualityStatus(electrodeSet, forearmTrapezoidpoints, weights, main_canvas);
         }
     }
 
